c6752bb6426abed07d9ff83f90eff27a486a2540
SivaKumar-004 <anandhusiva1234@gmail.com>
2025-06-20 21:21:26 +0530

    js change in carosel almost stable

--- js/main.js
+++ js/main.js
@@ -30,14 +30,13 @@
     $(".header-carousel").owlCarousel({
         animateOut: 'slideOutDown',
         items: 1,
-        autoplay: true,
+        autoplay: false,
         smartSpeed: 1000,
         dots: false,
-        loop: true,
+        loop: false,
         nav : true,
         navText : [
             '<i class="bi bi-arrow-left"></i>',
-            '<i class="bi bi-arrow-right"></i>'
         ],
     });
 
